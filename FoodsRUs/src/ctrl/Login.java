c14a1618854daf4a74c15ea30ba33e6417e85084
SiweiWang <wangsiwei2003@hotmail.com>
2012-11-09 18:06:00 -0500

    Add new servlet and page templates

---
+++ FoodsRUs/src/ctrl/Login.java
@@ -0,0 +1,63 @@
+package ctrl;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login, used for login 
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String login = request.getParameter("login");
+		String target;
+		HttpSession session = request.getSession();
+		if (login.isEmpty())
+		{
+			target = "/login.jspx";
+		}
+		else
+		{
+			if (login =="lgoin")
+			{
+				//check db and login
+				System.out.println("login");
+				boolean loginOK = false;
+
+				//TODO check and set loginOk
+				session.setAttribute("login", loginOK);		
+			}
+			target = "index.jspx";
+		}	
+		request.getRequestDispatcher(target).forward(request, response);
+
+	}
+		
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
