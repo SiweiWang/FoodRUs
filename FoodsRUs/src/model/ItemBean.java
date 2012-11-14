e62579d722b7ae5834ef715104d511628f0993bc
Derek <lide.xu@gmail.com>
2012-11-13 21:06:09 -0500

    ItemBean modified

--- FoodsRUs/src/model/ItemBean.java
+++ FoodsRUs/src/model/ItemBean.java
@@ -55,143 +55,163 @@ public class ItemBean {
 	/**
 	 * @return the itemNumber
 	 */
-	public String getItemNumber() {
+	public String getItemNumber()
+	{
 		return itemNumber;
 	}
 
 	/**
 	 * @param itemNumber the itemNumber to set
 	 */
-	public void setItemNumber(String itemNumber) {
+	public void setItemNumber(String itemNumber)
+	{
 		this.itemNumber = itemNumber;
 	}
 
 	/**
-	 * @return the itemName
+	 * @return the name
 	 */
-	public String getItemName() {
+	public String getName()
+	{
 		return name;
 	}
 
 	/**
-	 * @param itemName the itemName to set
+	 * @param name the name to set
 	 */
-	public void setItemName(String itemName) {
-		this.name = itemName;
+	public void setName(String name)
+	{
+		this.name = name;
 	}
 
 	/**
 	 * @return the price
 	 */
-	public double getPrice() {
+	public double getPrice()
+	{
 		return price;
 	}
 
 	/**
 	 * @param price the price to set
 	 */
-	public void setPrice(double price) {
+	public void setPrice(double price)
+	{
 		this.price = price;
 	}
 
 	/**
-	 * @return the catID
+	 * @return the quantity
 	 */
-	public int getCatID() {
-		return catID;
-	}
-
-	/**
-	 * @param catID the catID to set
-	 */
-	public void setCatID(int catID) {
-		this.catID = catID;
-	}
-
-	/**
-	 * @return the qty
-	 */
-	public int getQty() {
+	public int getQuantity()
+	{
 		return quantity;
 	}
 
 	/**
-	 * @param qty the qty to set
+	 * @param quantity the quantity to set
 	 */
-	public void setQty(int qty) {
-		this.quantity = qty;
-		this.setExtended(this.price * this.quantity);
+	public void setQuantity(int quantity)
+	{
+		this.quantity = quantity;
 	}
 
-	/**
-	 * @return the onOrder
-	 */
-	public int getOnOrder() {
-		return onOrder;
-	}
-
-	/**
-	 * @param onOrder the onOrder to set
-	 */
-	public void setOnOrder(int onOrder) {
-		this.onOrder = onOrder;
-	}
-
-	/**
-	 * @return the reOrder
-	 */
-	public int getReOrder() {
-		return reOrder;
-	}
-
-	/**
-	 * @param reOrder the reOrder to set
-	 */
-	public void setReOrder(int reOrder) {
-		this.reOrder = reOrder;
-	}
-
-	/**
-	 * @return the cost
-	 */
-	public double getCost() {
-		return cost;
-	}
-
-	/**
-	 * @param cost the cost to set
-	 */
-	public void setCost(double cost) {
-		this.cost = cost;
-	}
-
-	/**
-	 * @return the supID
-	 */
-	public int getSupID() {
-		return supID;
-	}
-
-	/**
-	 * @param supID the supID to set
-	 */
-	public void setSupID(int supID) {
-		this.supID = supID;
-	}
-	
 	/**
 	 * @return the extended
 	 */
-	public double getExtended() {
+	public double getExtended()
+	{
 		return extended;
 	}
 
 	/**
 	 * @param extended the extended to set
 	 */
-	private void setExtended(double extended) {
+	public void setExtended(double extended)
+	{
 		this.extended = extended;
 	}
 
+	/**
+	 * @return the catID
+	 */
+	public int getCatID()
+	{
+		return catID;
+	}
+
+	/**
+	 * @param catID the catID to set
+	 */
+	public void setCatID(int catID)
+	{
+		this.catID = catID;
+	}
+
+	/**
+	 * @return the onOrder
+	 */
+	public int getOnOrder()
+	{
+		return onOrder;
+	}
+
+	/**
+	 * @param onOrder the onOrder to set
+	 */
+	public void setOnOrder(int onOrder)
+	{
+		this.onOrder = onOrder;
+	}
+
+	/**
+	 * @return the reOrder
+	 */
+	public int getReOrder()
+	{
+		return reOrder;
+	}
+
+	/**
+	 * @param reOrder the reOrder to set
+	 */
+	public void setReOrder(int reOrder)
+	{
+		this.reOrder = reOrder;
+	}
+
+	/**
+	 * @return the cost
+	 */
+	public double getCost()
+	{
+		return cost;
+	}
+
+	/**
+	 * @param cost the cost to set
+	 */
+	public void setCost(double cost)
+	{
+		this.cost = cost;
+	}
+
+	/**
+	 * @return the supID
+	 */
+	public int getSupID()
+	{
+		return supID;
+	}
+
+	/**
+	 * @param supID the supID to set
+	 */
+	public void setSupID(int supID)
+	{
+		this.supID = supID;
+	}
+
+	
 
 }
