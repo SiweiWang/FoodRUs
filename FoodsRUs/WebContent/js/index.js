cc50e1660ba81f8c8bc2c1254427bbc6d2f8bf51
Serge <sergenhs@hotmail.com>
2012-11-19 21:39:57 -0500

    added javascript

---
+++ FoodsRUs/WebContent/js/index.js
@@ -0,0 +1,12 @@
+//checks whether the quantity to add in the form is zero
+function checkQty(form)
+{
+	var qtyToAdd = form.childNodes[1].value;
+	var ok = true;
+	if (qtyToAdd == 0)
+	{
+		alert("The quantity for the item you're trying to add is 0!");
+		ok = false;
+	}	
+	return ok;
+}
