3bb7c0bf780728613c84540f9a69d6a66927ce47
Serge <sergenhs@hotmail.com>
2012-11-21 17:14:53 -0500

    added js for shoppingcart

--- FoodsRUs/WebContent/js/index.js
+++ FoodsRUs/WebContent/js/index.js
@@ -1,4 +1,4 @@
-//checks whether the quantity to add in the form is zero
+//checks whether the quantity to add in the form is valid
 function checkQty(form)
 {
 	var qtyToAdd = form.childNodes[1].value;
@@ -7,8 +7,34 @@ function checkQty(form)
 	{
 		alert("The quantity for the item you're trying to add is 0!");
 		ok = false;
-	}	
+	}else if (qtyToAdd < 0)
+	{
+		alert("You're trying to add a negative quantity!");
+		ok = false;
+	}
 	return ok;
+}
+
+function checkUpdate()
+{
+	var q = document.getElementsByClassName("qtyInput");
+	for(var i=0; i <= q.length; i++)
+	{
+		if (q[i].value < 0)
+		{
+			alert("The quantity for one of the items is negative!");
+			return false;
+		}
+	}
+	return true;
+}
+
+function validateQty(qty)
+{
+	if(qty.value < 0)
+	{
+		alert("The quantity for one of the items is negative!");
+	}
 }
 
 function clearSearchBox(box)
